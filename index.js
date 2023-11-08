2930c8ae3302c752637cf877e195564acb7c7517
halimaislam1 <halimaislam6181@gmail.com>
2023-11-08 12:35:34 +0600

    get single service by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,6 +34,14 @@ async function run() {
     app.get('/services',async(req, res) => {
         const result = await serviceCollection.find().toArray();
         res.send(result)
+    })
+
+    //get single services by id
+    app.get('/services/:id',async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await serviceCollection.findOne(query)
+      res.send(result)
     })
     
     //post services
